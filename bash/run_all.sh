9eb96016f38a00c5d644132e2d22262006164247
Xu <xushengquan@souche.com>
2020-01-03 14:46:37 +0800

    endpoints

--- bash/run_all.sh
+++ bash/run_all.sh
@@ -4,4 +4,4 @@ cd ..
 
 python -m rasa_sdk.endpoint --actions actions
 
-rasa run --endpoints config/endpoints.yml --enable-api --m models/20191026-165940.tar.gz --debug
+rasa run --endpoints config/endpoints.yml --enable-api --m models/20191106-105047.tar.gz --debug
