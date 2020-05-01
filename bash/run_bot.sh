145e800ab9bc41fe433a84c756c8ae9bec76a03e
Xu <xushengquan@souche.com>
2020-05-01 16:22:35 +0800

    解决跨域请求问题

--- bash/run_bot.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd ..
-
-# python -m rasa_sdk.endpoint --actions actions
-
-nohup rasa run --endpoints config/endpoints.yml --enable-api --m models --debug > bot_log 2>&1 &
--- bash/start_rasa.sh
+++ bash/start_rasa.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 cd ..
-rasa run --endpoints config/endpoints.yml --enable-api --m models/20191004-145426.tar.gz --debug
+rasa run --endpoints config/endpoints.yml --enable-api --m models --log-file logs/rasa_bot.log --debug --cors "*"
+
+# rasa run -m models --enable-api --log-file out.log -p 5500 --cors "*"
