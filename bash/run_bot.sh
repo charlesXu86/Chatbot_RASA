3f106c99a29fd5a1dfac7f4c552ef292600a8168
Xu <charlesxu86@163.com>
2020-06-30 22:37:04 +0800

    优化

--- bash/run_bot.sh
+++ bash/run_bot.sh
@@ -3,5 +3,4 @@
 cd ..
 
 # python -m rasa_sdk.endpoint --actions actions
-
-nohup rasa run --endpoints config/endpoints.yml --enable-api --m models --debug > $(dirname $(pwd))/Chatbot_RASA/logs/bot.log 2>&1 &
+rasa run --endpoints config/endpoints.yml --enable-api --m models -p 9008 --debug --cors "*" > $(dirname $(pwd))/Chatbot_RASA/logs/bot.log 2>&1 &
--- run_action.py
+++ run_action.py
@@ -13,29 +13,6 @@
  
 """
 
-#!/usr/bin/env python
+import rasa_sdk
 
-# -*- coding: utf-8 -*-
 
-from pudb import set_trace
-
-set_trace()
-
-def min(a,b):
-    minValue = 0
-    if(a >= b):
-        minValue = b
-    else:
-        minValue = a
-    return minValue
-
-if __name__ == "__main__":
-    a = 1
-    b = 10
-    minValue = 0
-    while(a < b):
-        minValue = min(a,b)
-
-        print("minValue=",minValue)
-        a += 1
-        print("a=",a)
