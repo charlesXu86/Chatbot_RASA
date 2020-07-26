31f0ac4bed064d9ab7d6a821d8c6e3d5ec01ed4f
Xu <xushengquan@souche.com>
2020-07-26 11:15:23 +0800

    config

--- bash/rasa_train_dm_tf2.sh
+++ bash/rasa_train_dm_tf2.sh
@@ -2,3 +2,5 @@
 
 cd ..
 rasa train --domain domain/domain.yml --data data --out models core --config config/config_tf2.yml
+
+rasa train --domain domain.yml --data data --out models core --config config/config_tf2.yml
